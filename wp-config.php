74f839ba0634d3346acc03be83a6d9950d2cf948
Charles RLG <charlesrabeclegloahec@gmail.com>
2022-09-20 15:08:13 +0200

    Modif config

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'SCALINGO_MYSQL_URL' );
+define( 'DB_NAME', '$SCALINGO_MYSQL_URL' );
 
 /** Utilisateur de la base de données MySQL. */
 define( 'DB_USER', 'necstrasbou_5945' );
