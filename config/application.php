9758450acf7d07abef284bafaa8346fb4501743b
Charles RLG <charlesrabeclegloahec@gmail.com>
2022-09-21 10:46:36 +0200

    config

--- config/application.php
+++ config/application.php
@@ -1,28 +1,28 @@
 <?php
 
-$mysql_url = parse_url($_ENV["DATABASE_URL"]);
-$db = substr($mysql_url['path'], 1);
+// $mysql_url = parse_url($_ENV["DATABASE_URL"]);
+// $db = substr($mysql_url['path'], 1);
 
-Config::define('DB_NAME', $SCALINGO_MYSQL_URL);
-Config::define('DB_USER', $mysql_url['necstrasbou_5945']);
-Config::define('DB_PASSWORD', $mysql_url['xxteRuIgmieQJoLZZvyf']);
-Config::define('DB_HOST', $mysql_url['mysql://necstrasbou_5945:xxteRuIgmieQJoLZZvyf@80b6837a-3141-427c-bd5a-00596b6dbdd7.necstrasbou-5945.mysql.a.osc-fr1.scalingo-dbs.com:39303/necstrasbou_5945?useSSL=true&verifyServerCertificate=false'] . ":" . $mysql_url['8080']);
+// Config::define('DB_NAME', $SCALINGO_MYSQL_URL);
+// Config::define('DB_USER', $mysql_url['necstrasbou_5945']);
+// Config::define('DB_PASSWORD', $mysql_url['xxteRuIgmieQJoLZZvyf']);
+// Config::define('DB_HOST', $mysql_url['mysql://necstrasbou_5945:xxteRuIgmieQJoLZZvyf@80b6837a-3141-427c-bd5a-00596b6dbdd7.necstrasbou-5945.mysql.a.osc-fr1.scalingo-dbs.com:39303/necstrasbou_5945?useSSL=true&verifyServerCertificate=false'] . ":" . $mysql_url['8080']);
 
-$key = $_ENV["SECURE_KEY"];
-Config::define('AUTH_KEY',         $key);
-Config::define('SECURE_AUTH_KEY',  $key);
-Config::define('LOGGED_IN_KEY',    $key);
-Config::define('NONCE_KEY',        $key);
-Config::define('AUTH_SALT',        $key);
-Config::define('SECURE_AUTH_SALT', $key);
-Config::define('LOGGED_IN_SALT',   $key);
-Config::define('NONCE_SALT',       $key);
+// $key = $_ENV["SECURE_KEY"];
+// Config::define('AUTH_KEY',         $key);
+// Config::define('SECURE_AUTH_KEY',  $key);
+// Config::define('LOGGED_IN_KEY',    $key);
+// Config::define('NONCE_KEY',        $key);
+// Config::define('AUTH_SALT',        $key);
+// Config::define('SECURE_AUTH_SALT', $key);
+// Config::define('LOGGED_IN_SALT',   $key);
+// Config::define('NONCE_SALT',       $key);
 
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
-  $_SERVER['HTTPS'] = 'on';
+// if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+//   $_SERVER['HTTPS'] = 'on';
 
-Config::define('AUTOMATIC_UPDATER_DISABLED', true);
+// Config::define('AUTOMATIC_UPDATER_DISABLED', true);
 
-Config::define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL);
+// Config::define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL);
 
 ?>
