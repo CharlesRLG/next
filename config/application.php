f7400f5d27a4d58cef819325a8a7a8a811e8f4c0
Charles RLG <charlesrabeclegloahec@gmail.com>
2022-09-21 09:25:07 +0200

    config

--- config/application.php
+++ config/application.php
@@ -3,10 +3,10 @@
 $mysql_url = parse_url($_ENV["DATABASE_URL"]);
 $db = substr($mysql_url['path'], 1);
 
-Config::define('DB_NAME', $SCALINGO_MYSQL_URL);
-Config::define('DB_USER', $mysql_url['necstrasbou_5945']);
-Config::define('DB_PASSWORD', $mysql_url['xxteRuIgmieQJoLZZvyf']);
-Config::define('DB_HOST', $mysql_url['mysql://necstrasbou_5945:xxteRuIgmieQJoLZZvyf@80b6837a-3141-427c-bd5a-00596b6dbdd7.necstrasbou-5945.mysql.a.osc-fr1.scalingo-dbs.com:39303/necstrasbou_5945?useSSL=true&verifyServerCertificate=false'] . ":" . $mysql_url['8080']);
+// Config::define('DB_NAME', $SCALINGO_MYSQL_URL);
+// Config::define('DB_USER', $mysql_url['necstrasbou_5945']);
+// Config::define('DB_PASSWORD', $mysql_url['xxteRuIgmieQJoLZZvyf']);
+// Config::define('DB_HOST', $mysql_url['mysql://necstrasbou_5945:xxteRuIgmieQJoLZZvyf@80b6837a-3141-427c-bd5a-00596b6dbdd7.necstrasbou-5945.mysql.a.osc-fr1.scalingo-dbs.com:39303/necstrasbou_5945?useSSL=true&verifyServerCertificate=false'] . ":" . $mysql_url['8080']);
 
 $key = $_ENV["SECURE_KEY"];
 Config::define('AUTH_KEY',         $key);
