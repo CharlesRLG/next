9f1f890e5959389a62204b607f91e60f2e4b4f45
Charles RLG <charlesrabeclegloahec@gmail.com>
2022-09-21 10:35:31 +0200

    config

--- config/application.php
+++ config/application.php
@@ -1,28 +1,142 @@
 <?php
+/**
+ * Your base production configuration goes in this file. Environment-specific
+ * overrides go in their respective config/environments/{{WP_ENV}}.php file.
+ *
+ * A good default policy is to deviate from the production config as little as
+ * possible. Try to define as much of your configuration in this file as you
+ * can.
+ */
 
-$mysql_url = parse_url($_ENV["DATABASE_URL"]);
-$db = substr($mysql_url['path'], 1);
+use Roots\WPConfig\Config;
 
-Config::define('DB_NAME', $SCALINGO_MYSQL_URL);
-Config::define('DB_USER', $mysql_url['necstrasbou_5945']);
-Config::define('DB_PASSWORD', $mysql_url['xxteRuIgmieQJoLZZvyf']);
-Config::define('DB_HOST', $mysql_url['mysql://necstrasbou_5945:xxteRuIgmieQJoLZZvyf@80b6837a-3141-427c-bd5a-00596b6dbdd7.necstrasbou-5945.mysql.a.osc-fr1.scalingo-dbs.com:39303/necstrasbou_5945?useSSL=true&verifyServerCertificate=false'] . ":" . $mysql_url['8080']);
+/** @var string Directory containing all of the site's files */
+$root_dir = dirname(__DIR__);
 
-$key = $_ENV["SECURE_KEY"];
-Config::define('AUTH_KEY',         $key);
-Config::define('SECURE_AUTH_KEY',  $key);
-Config::define('LOGGED_IN_KEY',    $key);
-Config::define('NONCE_KEY',        $key);
-Config::define('AUTH_SALT',        $key);
-Config::define('SECURE_AUTH_SALT', $key);
-Config::define('LOGGED_IN_SALT',   $key);
-Config::define('NONCE_SALT',       $key);
+/** @var string Document Root */
+$webroot_dir = $root_dir . '/web';
 
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
-  $_SERVER['HTTPS'] = 'on';
+/**
+ * Expose global env() function from oscarotero/env
+ */
+Env::init();
 
+/**
+ * Use Dotenv to set required environment variables and load .env file in root
+ */
+$dotenv = Dotenv\Dotenv::create($root_dir);
+if (file_exists($root_dir . '/.env')) {
+    $dotenv->load();
+    $dotenv->required(['WP_HOME', 'WP_SITEURL']);
+    if (!env('DATABASE_URL')) {
+        $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD']);
+    }
+}
+
+/**
+ * Set up our global environment constant and load its config first
+ * Default: production
+ */
+define('WP_ENV', env('WP_ENV') ?: 'production');
+
+/**
+ * URLs
+ */
+Config::define('WP_HOME', env('WP_HOME'));
+Config::define('WP_SITEURL', env('necstrasbourg.fr'));
+
+/**
+ * Custom Content Directory
+ */
+Config::define('CONTENT_DIR', '/app');
+Config::define('WP_CONTENT_DIR', $webroot_dir . Config::get('CONTENT_DIR'));
+Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
+
+/**
+ * DB settings
+ */
+Config::define('DB_NAME', env('$SCALINGO_MYSQL_URL'));
+Config::define('DB_USER', env('necstrasbou_5945'));
+Config::define('DB_PASSWORD', env('xxteRuIgmieQJoLZZvyf'));
+Config::define('DB_HOST', env('mysql://necstrasbou_5945:xxteRuIgmieQJoLZZvyf@80b6837a-3141-427c-bd5a-00596b6dbdd7.necstrasbou-5945.mysql.a.osc-fr1.scalingo-dbs.com:39303/necstrasbou_5945?useSSL=true&verifyServerCertificate=false') ?: '8080');
+Config::define('DB_CHARSET', 'utf8mb4');
+Config::define('DB_COLLATE', '');
+$table_prefix = env('DB_PREFIX') ?: 'wp_';
+
+if (env('DATABASE_URL')) {
+    $dsn = (object) parse_url(env('DATABASE_URL'));
+
+    Config::define('DB_NAME', substr($dsn->path, 1));
+    Config::define('DB_USER', $dsn->user);
+    Config::define('DB_PASSWORD', isset($dsn->pass) ? $dsn->pass : null);
+    Config::define('DB_HOST', isset($dsn->port) ? "{$dsn->host}:{$dsn->port}" : $dsn->host);
+}
+
+/**
+ * Authentication Unique Keys and Salts
+ */
+Config::define('AUTH_KEY', env('AUTH_KEY'));
+Config::define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
+Config::define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
+Config::define('NONCE_KEY', env('NONCE_KEY'));
+Config::define('AUTH_SALT', env('AUTH_SALT'));
+Config::define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
+Config::define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
+Config::define('NONCE_SALT', env('NONCE_SALT'));
+
+/**
+ * S3 Uploads Plugin
+ */
+Config::define('S3_UPLOADS_BUCKET', env('S3_UPLOADS_BUCKET'));
+Config::define('S3_UPLOADS_KEY', env('S3_UPLOADS_KEY'));
+Config::define('S3_UPLOADS_SECRET', env('S3_UPLOADS_SECRET'));
+Config::define('S3_UPLOADS_REGION', env('S3_UPLOADS_REGION'));
+if (env('S3_UPLOADS_HTTP_CACHE_CONTROL')) {
+    Config::define('S3_UPLOADS_HTTP_CACHE_CONTROL', env('S3_UPLOADS_HTTP_CACHE_CONTROL'));
+}
+if (env('S3_UPLOADS_HTTP_EXPIRES')) {
+    Config::define('S3_UPLOADS_HTTP_EXPIRES', env('S3_UPLOADS_HTTP_EXPIRES'));
+}
+if (env('S3_UPLOADS_BUCKET_URL')) {
+    Config::define('S3_UPLOADS_BUCKET_URL', env('S3_UPLOADS_BUCKET_URL'));
+}
+
+/**
+ * Custom Settings
+ */
 Config::define('AUTOMATIC_UPDATER_DISABLED', true);
+Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
+// Disable the plugin and theme file editor in the admin
+Config::define('DISALLOW_FILE_EDIT', true);
+// Disable plugin and theme updates and installation from the admin
+Config::define('DISALLOW_FILE_MODS', true);
 
-Config::define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL);
+/**
+ * Debugging Settings
+ */
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('SCRIPT_DEBUG', false);
+ini_set('display_errors', '0');
 
-?>
+/**
+ * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
+ * See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
+ */
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
+$env_config = __DIR__ . '/environments/' . WP_ENV . '.php';
+
+if (file_exists($env_config)) {
+    require_once $env_config;
+}
+
+Config::apply();
+
+/**
+ * Bootstrap WordPress
+ */
+if (!defined('ABSPATH')) {
+    define('ABSPATH', $webroot_dir . '/wp/');
+}
